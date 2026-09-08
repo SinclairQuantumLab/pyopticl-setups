c400479655f41356086164ffb64119c1afb29840
mkim129 <m129kim@gmail.com>
2026-09-07 22:41:59 -0500

    Added micro lenses

--- Testboards/Cavity test.py
+++ Testboards/Cavity test.py
@@ -1,6 +1,8 @@
 from PyOpticL.beam_path import BeamPath
 from PyOpticL.layout import Component
 from PyOpticL.library import Baseplate
+from PyOpticL.library import optics
+from PyOpticL.library.optics import micro_lens
 from PyOpticL.library.IMAQ_library import *
 from PyOpticL.utils import Dimension as dim
 from PyOpticL.utils import cardinal_angle, turn_angle
@@ -13,12 +15,9 @@ gap = dim(1 / 8, "in")
 
 mount_holes = [
     (1, 1),
-    (1, base_dy - dim(1, "in")),
-    (base_dx - dim(1, "in"), 1),
-    (base_dx - dim(1, "in"), base_dy - dim(1, "in")),
 ]
 
-input_x = dim(5, "in")
+input_x = dim(100, "mm")
 input_y = dim(5, "in")
 
 
@@ -38,47 +37,169 @@ def testboard(label: str = "Testboard"):
         BeamPath(
             label="beam",
             wavelength=780.24,
-            waist=dim(3, "mm"),
+            waist=dim(1.25, "in"),
             final_distance=dim(12, "in"),
         ),
-        position=(input_x, input_y, 0),
+        position=(input_x, input_y, dim(1, "in")),
         rotation=cardinal_angle["right"],
     )
 
-    # Laser input
+
+    beam.add(
+        Component(
+            label="Cavity1",
+            definition=optics.Cavity_Mirror(
+                diameter=dim(3, "in"),
+                ref_ratio=0.99,
+                input=True,
+            ),
+        ),
+        beam_index=0b1,
+        distance=dim(50, "mm"),
+        rotation=cardinal_angle["right"],
+    )
+
+
     baseplate.add(
-        ips_small("Laser"),
-        position=(input_x, input_y, 0),
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(250, "mm"), dim(5, "in"), dim(1, "in")),
         rotation=cardinal_angle["right"],
     )
 
-    beam.add(
-        cavity("Cavity1",input=True),
-        beam_index=0b1,
-        distance=dim(4, "in"),
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(250, "mm"), dim(4.5, "in"), dim(1, "in")),
         rotation=cardinal_angle["right"],
     )
 
-    beam.add(
-        lens_50("Lens 50"),
-        beam_index=0b1,
-        distance=dim(100, "mm"),
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(250, "mm"), dim(5.5, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(250, "mm"), dim(4, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(250, "mm"), dim(6, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
+
+
+    baseplate.add(
+        Component(
+            label="Cavity2",
+            definition=optics.Cavity_Mirror(
+                diameter=dim(3, "in"),
+                ref_ratio=0.99,
+                input=False,
+            ),
+        ),
+        position=(dim(300, "mm"), dim(5, "in"), dim(1, "in")),
         rotation=cardinal_angle["left"],
     )
 
-    beam.add(
-        cavity("Cavity2", input=False),
-        beam_index=0b1,
-        distance=dim(50, "mm"),
-        rotation=cardinal_angle["left"],
-    )
 
-    beam.add(
-        lens_50("Lens 50"),
-        beam_index=0b10,
-        distance=dim(50, "mm"),
+
+
+
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(350, "mm"), dim(5, "in"), dim(1, "in")),
         rotation=cardinal_angle["right"],
     )
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(350, "mm"), dim(4.5, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(350, "mm"), dim(5.5, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(350, "mm"), dim(4, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
+    baseplate.add(
+        Component(
+            label="Micro lens f=50 mm",
+            definition=micro_lens(
+                focal_length=dim(50, "mm"),
+                part_number="micro_lens f=50 mm",
+            ),
+        ),
+        position=(dim(350, "mm"), dim(6, "in"), dim(1, "in")),
+        rotation=cardinal_angle["right"],
+    )
+
 
 
     return baseplate
